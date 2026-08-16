39b560bb3b3e0a05f3afa0705d39d36d9a5946d6
karch <karch00@proton.me>
2026-08-16 04:59:53 +0200

    Removed unused modules:

--- eww/scripts/weather/switch.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-weather=$(eww get displayWeather)
-
-if [[ "$weather" == "hour" ]]; then
-    eww update displayWeather="day"
-else
-    eww update displayWeather="hour"
-fi
--- sddm/themes/karch/colors.js
+++
@@ -1,4 +0,0 @@
-var background = '#2c2432'
-var foreground = '#fae6cb'
-var border = '#2c2432'
-var clock = '#fae6cb'
